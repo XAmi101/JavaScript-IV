7fb24fd6bc4aa4fac39d258813cb0b09d11706e9
XAmi101 <famousvacancy@gmail.com>
2019-06-13 18:03:45 -0400

    Prototype refactor finished now working on class assgnment

--- assignments/prototype-refactor.js
+++ assignments/prototype-refactor.js
@@ -76,7 +76,7 @@ class GameObject {
 
 // Test you work by un-commenting these 3 objects and the list of console logs below:
 
-/*
+
 
  const mage = new Humanoid({
     createdAt: new Date(),
@@ -138,10 +138,3 @@ class GameObject {
     console.log(archer.greet()); // Lilith offers a greeting in Elvish.
     console.log(mage.takeDamage()); // Bruce took damage.
     console.log(swordsman.destroy()); // Sir Mustachio was removed from the game.
-
-    // * Create Villain and Hero constructor functions that inherit from the Humanoid constructor function.
-      // * Give the Hero and Villains different methods that could be used to remove health points from objects which could result in destruction if health gets to 0 or drops below 0;
-      // * Create two new objects, one a villain and one a hero and fight it out with methods!
-
-
-*/
